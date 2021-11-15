2e0b333cc4ced55263fb1c53b2c1f4cc082aa6fa
João Victor <51753615+Joo-pipopo@users.noreply.github.com>
2021-11-16 00:51:34 +0300

    atualização de banco

--- www/js/firebase.js
+++ www/js/firebase.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-  apiKey: "AIzaSyCem1hc3BUG2b2Lp9uOlP6WnyDT_6YpXPA",
-  authDomain: "wazde-4ef1e.firebaseapp.com",
-  projectId: "wazde-4ef1e",
-  storageBucket: "wazde-4ef1e.appspot.com",
-  messagingSenderId: "585468585196",
-  appId: "1:585468585196:web:0a073d83bb1ebdba01b1c9",
-  measurementId: "G-VPBMFKYCJW"
+  apiKey: "AIzaSyDYyaIq38xJN6_ij01Hsv-0jDF6l1f1jIo",
+  authDomain: "wazerunner-dd26c.firebaseapp.com",
+  projectId: "wazerunner-dd26c",
+  storageBucket: "wazerunner-dd26c.appspot.com",
+  messagingSenderId: "213719174051",
+  appId: "1:213719174051:web:d61ad00334b94b7b412816",
+  measurementId: "G-KH2Q5ZEJHB"
 };
 
 // Initialize Firebase
