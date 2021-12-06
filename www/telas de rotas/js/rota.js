a3abce2097df0f92faf6524e7ecc305b1867d2be
Gabriel L. Tecco <51713625+Tecco2233@users.noreply.github.com>
2021-12-06 05:34:57 -0300

    Add files via upload

--- www/telas de rotas/js/rota.js
+++ www/telas de rotas/js/rota.js
@@ -16,7 +16,7 @@ let lat;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: -23.588333, lng: -46.658890 },
     zoom: 18,
   });
 
